5eecfb730207c50dc2c2f1d9b6088eeb9e0a639b
emmavanoss <e.vanoss@gmail.com>
2023-08-04 10:13:25 +0100

    Add colors

--- warmAutumn.ts
+++ warmAutumn.ts
@@ -2,12 +2,236 @@ import type { Color } from "./types"
 
 // icolorpalette.com
 const colors: Color[] = [
+    // {
+    //     pantoneName: "",
+    //     pantoneCode: "",
+    //     hex: "",
+    //     season: "Warm Autumn",
+    // },
+
+    // creamy neutrals
+    {
+        pantoneName: "Seedpearl",
+        pantoneCode: "12-0703",
+        hex: "#E6DAC4",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Angora",
+        pantoneCode: "12-0605",
+        hex: "#DFD1BB",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Fog",
+        pantoneCode: "13-0607",
+        hex: "#D0C5B1",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Double Cream",
+        pantoneCode: "12-0715",
+        hex: "#F3E0AC",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Straw",
+        pantoneCode: "13-0922",
+        hex: "#E0C992",
+        season: "Warm Autumn",
+    },
+
+    // greenish neutrals
+    {
+        pantoneName: "Tidal Foam",
+        pantoneCode: "14-0210",
+        hex: "#BFB9A3",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Oil Green",
+        pantoneCode: "17-0115",
+        hex: "#80856D",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Lead Gray",
+        pantoneCode: "17-1118",
+        hex: "#8A7963",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Stone Gray",
+        pantoneCode: "18-0615",
+        hex: "#685E4F",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Deep Depths",
+        pantoneCode: "19-0413",
+        hex: "#46483D",
+        season: "Warm Autumn",
+    },
+
+    // peachy neutrals
+    {
+        pantoneName: "Peach Nougat",
+        pantoneCode: "14-1220",
+        hex: "#E6AF91",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Peach Bloom",
+        pantoneCode: "15-1327",
+        hex: "#D99B7C",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Cedar Wood",
+        pantoneCode: "17-1525",
+        hex: "#A1655B",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Henna",
+        pantoneCode: "19-1334",
+        hex: "#7C423C",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Chicory Coffee",
+        pantoneCode: "19-1419",
+        hex: "#4B342F",
+        season: "Warm Autumn",
+    },
+
+    // oranges
+    {
+        pantoneName: "Muskmelon",
+        pantoneCode: "15-1242",
+        hex: "#EC935E",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Amberglow",
+        pantoneCode: "16-1350",
+        hex: "#DC793E",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Orange Rust",
+        pantoneCode: "18-1447",
+        hex: "#C25A3C",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Summer Fig",
+        pantoneCode: "17-1450",
+        hex: "#BE4B3B",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Picante",
+        pantoneCode: "19-1250",
+        hex: "#8D3F2D",
+        season: "Warm Autumn",
+    },
+
+    // reds
     {
         pantoneName: "Peach Amber",
         pantoneCode: "15-1423",
         hex: "#FB9F93",
         season: "Warm Autumn",
     }
+    {
+        pantoneName: "Porcelain Rose",
+        pantoneCode: "17-1643",
+        hex: "#EA6B6A",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "American Beauty",
+        pantoneCode: "19-1759",
+        hex: "#A73340",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Merlot",
+        pantoneCode: "19-1534",
+        hex: "#73272D",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Maroon Banner",
+        pantoneCode: "19-1529",
+        hex: "#5B2B38",
+        season: "Warm Autumn",
+    },
+
+    // blues
+    {
+        pantoneName: "Sea Jet",
+        pantoneCode: "15-4713",
+        hex: "#54AFBC",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Bayou",
+        pantoneCode: "18-5121",
+        hex: "#20706F",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Blue Mist",
+        pantoneCode: "16-4421",
+        hex: "#5BACC3",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Biscay Bay",
+        pantoneCode: "18-4726",
+        hex: "#097988",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Deep Teal",
+        pantoneCode: "19-4914",
+        hex: "#18454B",
+        season: "Warm Autumn",
+    },
+
+    // greens
+    {
+        pantoneName: "Beechnut",
+        pantoneCode: "14-0425",
+        hex: "#C2C18D",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Fern",
+        pantoneCode: "16-0430",
+        hex: "#9AA067",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Split Pea",
+        pantoneCode: "16-0545",
+        hex: "#A29F4B",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Fir Green",
+        pantoneCode: "18-0627",
+        hex: "#67592A",
+        season: "Warm Autumn",
+    },
+    {
+        pantoneName: "Forest Night",
+        pantoneCode: "18-0414",
+        hex: "#434237",
+        season: "Warm Autumn",
+    },
 
 ]
 
--- warmSpring.ts
+++ warmSpring.ts
@@ -1,12 +1,180 @@
 import type { Color } from "./types"
 
-// icolorpalette.com
+// palette: https://theconceptwardrobe.com/colour-analysis-comprehensive-guides/true-spring-a-comprehensive-guide
+// hex codes (1): icolorpalette.com
+// hex codes (2): https://encycolorpedia.com/cfb686
+
 const colors: Color[] = 
 [
+    // neutrals
+    {
+        pantoneName: "Vanilla",
+        pantoneCode: "12-0712",
+        hex: "#F4E7C6",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Sun Kiss",
+        pantoneCode: "12-0807",
+        hex: "#EBD4BF",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Golden Fleece",
+        pantoneCode: "12-0822",
+        hex: "#F2D1A0",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Jojoba",
+        pantoneCode: "14-0935",
+        hex: "#DABE81",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Desert Mist",
+        pantoneCode: "14-1127",
+        hex: "#E0B589",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Golden Mist",
+        pantoneCode: "13-0624",
+        hex: "#D5CD94",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Moonstone",
+        pantoneCode: "14-1026",
+        hex: "#CFB686",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Pale Gold",
+        pantoneCode: "15-0927",
+        hex: "#BD9865",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Dachsund",
+        pantoneCode: "18-1033",
+        hex: "#704F37",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Demitasse",
+        pantoneCode: "19-0712",
+        hex: "#40342B",
+        season: "Warm Spring",
+    },
+
+    // yellows
+    {
+        pantoneName: "Impala",
+        pantoneCode: "13-1025",
+        hex: "#F8CE97",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Sunshine",
+        pantoneCode: "12-0727",
+        hex: "#FADE85",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Yarrow",
+        pantoneCode: "12-0758",
+        hex: "#FACE6D",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Mimosa",
+        pantoneCode: "14-0848",
+        hex: "#F0C05A",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Amber",
+        pantoneCode: "14-1045",
+        hex: "#EFAD55",
+        season: "Warm Spring",
+    },
+
+    // pinks
     {
         pantoneName: "Burnt Coral",
         pantoneCode: "16-1592",
         hex: "#E9897E",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Emberglow",
+        pantoneCode: "17-1547",
+        hex: "#EA6759",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Cayenne",
+        pantoneCode: "18-1651",
+        hex: "#E04951",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Georgia Peach",
+        pantoneCode: "16-1641",
+        hex: "#F97272",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Hibiscus",
+        pantoneCode: "18-1762",
+        hex: "#DD3848",
+        season: "Warm Spring",
+    },
+
+    // blues
+    {
+        pantoneName: "Windsurfer",
+        pantoneCode: "15-4031",
+        hex: "#A3BBE0",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Della Robbia Blue",
+        pantoneCode: "16-4020",
+        hex: "#7A9DCB",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Marina",
+        pantoneCode: "17-4041",
+        hex: "#4F84C4",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Directoire Blue",
+        pantoneCode: "18-4244",
+        hex: "#0061A3",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "True Blue",
+        pantoneCode: "19-4057",
+        hex: "#1E4477",
+        season: "Warm Spring",
+    },
+
+    // greens
+    {
+        pantoneName: "Spring Bouquet",
+        pantoneCode: "14-6340",
+        hex: "#6DCE87",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Irish Green",
+        pantoneCode: "15-6340",
+        hex: "#45BE76",
         season: "Warm Spring",
     },
     {
@@ -15,7 +183,49 @@ const colors: Color[] =
         hex: "#47C6A3",
         season: "Warm Spring",
     },
-
+    {
+        pantoneName: "Holly Green",
+        pantoneCode: "16-5932",
+        hex: "#0F9D76",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Bosphorus",
+        pantoneCode: "18-5633",
+        hex: "#007558",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Daiquiri Green",
+        pantoneCode: "12-0435",
+        hex: "#C9D77E",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Bright Chartreuse",
+        pantoneCode: "14-0445",
+        hex: "#B5BF50",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Titanite",
+        pantoneCode: "16-0229",
+        hex: "#8FAB3A",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "Salted Lime",
+        pantoneCode: "17-0232",
+        hex: "#60843C",
+        season: "Warm Spring",
+    },
+    {
+        pantoneName: "First Tee",
+        pantoneCode: "18-6031",
+        // couldn't find hex for First Tee online, used digital color meter + pantone website
+        hex: "#0C612C",
+        season: "Warm Spring",
+    },
 ]
 
 export default colors
